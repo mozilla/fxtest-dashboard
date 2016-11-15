0fe63b8993f0e6d51e103704a0821a8907b8644a
Kimberly Pennington <kimberlythegeek@gmail.com>
2016-11-14 23:13:25 -0700

    working on filter functionality

--- js/app.js
+++ js/app.js
@@ -4,17 +4,35 @@
 
     angular.module('dashboardApp', ['angularMoment'])
         .controller('FeedController', FeedController)
-        .controller('RepoController', RepoController)
         .constant('REPO_CONFIG_URL', 'config-new.json')
         .constant('API_BASE_PATH', 'https://api.github.com/repos/')
         .directive('githubIssue', githubIssue)
         .directive('githubRepository', githubRepository)
+        .filter('hasAssignee', hasAssignee)
         .service('GithubIssueFeedService', GithubIssueFeedService);
 
         FeedController.$inject = ['$scope', '$http', '$q', 'GithubIssueFeedService'];
         function FeedController($scope, $http, $q, GithubIssueFeedService) {
             var feed = this;
+            // Time for "Last Updated".
+            feed.time = new Date();
+            // Is this the pull requests page?
             $scope.pullRequest = false;
+            // Array for Available Filters
+            $scope.labels = [
+                'enhancement',
+                'help wanted',
+                'question',
+                'blocked',
+                'beginner',
+                'intermediate',
+                'advanced',
+                'urgent'
+            ];
+            $scope.filters = {
+                filterHasAssignee: null,
+                labels: []
+            };
 
             var promise = GithubIssueFeedService.getRepoList();
 
@@ -36,6 +54,7 @@
                         console.log('An error has occurred while fetching repository issues.');
                     });
                 });
+                console.log(feed.repos);
             }, function error(response) {
                 console.log('An error has occurred while fetching list of repositories.');
             });
@@ -44,12 +63,22 @@
             feed.hasPullRequests = function(repo) {
                 var foundPR = false;
                 angular.forEach(repo.issues, function(issue) {
-                    console.log(issue.pull_request);
                     if(issue.pull_request) {
                         foundPR = true;
                     }
                 });
                 return foundPR;
+            }
+
+            feed.countPullRequests = function(repo) {
+                if (feed.hasPullRequests(repo)) {
+                    var pullRequests = repo.issues.filter(function(item) {
+                        return item.pull_request !== undefined;
+                    });
+                    return pullRequests.length;
+                } else {
+                    return 0;
+                }
             }
 
             $scope.pullRequestFalse = function() {
@@ -61,12 +90,6 @@
             }
         }
 
-        RepoController.$inject = ['GithubIssueFeedService'];
-        function RepoController(GithubIssueFeedService) {
-
-
-
-        }
 
         function githubIssue(){
             return {
@@ -79,6 +102,13 @@
             return {
                 restrict: 'E',
                 templateUrl: 'templates/github-repository.html'
+            };
+        }
+
+        function hasAssignee() {
+            console.log(filter);
+            return function(issue) {
+                return issue.assignee !== null;
             };
         }
 
